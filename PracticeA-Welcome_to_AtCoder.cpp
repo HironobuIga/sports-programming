8584ad404479d82f9719ba7224b204853177ba64
hironobu-iga <hironobu.iga.developer@gmail.com>
2018-06-10 19:01:21 +0900

    [update]add cpp answer of AtCoder's practiceA - Welcome to atcoder

---
+++ PracticeA-Welcome_to_AtCoder.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    int b,c;
+    cin >> b >> c;
+    string s;
+    cin >> s;
+
+    int ans = a + b + c;
+    cout << ans <<  " " << s << endl;
+
+    return 0;
+}
