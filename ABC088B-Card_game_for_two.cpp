9db19aa52096761f118a8103fc9890b7619be9c8
hironobu-iga <hironobu.iga.developer@gmail.com>
2018-06-11 23:29:38 +0900

    [update]add C++ answer of AtCoder's ABC088B - Card Game for Two

---
+++ ABC088B-Card_game_for_two.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int card[N];
+
+    for (int i = 0; i < N; ++i) {
+        cin >> card[i];
+    }
+
+    int Alice = 0;
+    int Bob = 0;
+
+    sort(card, card + N, greater<int>()); // 昇順ソート 
+    for(int i = 0; i < N; ++i) {
+        if (i % 2 == 0) {
+            // 偶数番目はアリスのターン
+            Alice = Alice + card[i];
+        } else {
+            // 奇数番目はボブのターン
+            Bob = Bob + card[i];
+        }
+    }
+
+    int answer = Alice - Bob;
+
+    cout << answer;
+    return 0;
+}
