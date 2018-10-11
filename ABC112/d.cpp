c512922a07267f5fb741488f6cb22947d60f08a0
iganin <hironobu.iga.developer@gmail.com>
2018-10-11 23:41:06 +0900

    [update]abc112 d answer

--- ABC112/d.cpp
+++ ABC112/d.cpp
@@ -1,51 +1,38 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
-#include <math.h>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
 #define li long long int
-#define FOR(i, a, b) for (int i = (a) ; i < (b) ; ++i)
+#define FOR(i, a, b) for (li (i) = (a) ; (i) < (b) ; ++(i))
 #define REP(i, n) FOR(i, 0, n)
-
-// 素因数分解
-vector<li> decomposite(li n) {
-    li a = 2;
-    vector<li> ret;
-    while (n >= a * a) {
-        if (n % a == 0) {
-            n /= a;
-            ret.push_back(a);
-        } else {
-            a += 1;
-        }
-    }
-    return ret;
-}
-li gdc_euclid(li u, li v) {
-    while(v != 0) {
-        li r;
-        r = u % v; u = v; v = r;
-    }
-    return u;
-}
 
 int main(void) {
     li N, M;
     cin >> N >> M;
 
-    vector<li> dec = decomposite(M);
-    li euc;
-    euc = gdc_euclid(N, M);
-    cout << euc << endl;
-
-    li d = 1;
-    for(auto it = dec.begin(); d < N && it != dec.end(); ++it) {
-        d = d * (*it);
+    vector<li> d;
+    li sqrtM = sqrt(M);
+    FOR(i, 1, sqrtM + 1) {
+        if (M % i == 0) {
+            d.push_back(i);
+            d.push_back(M / i);
+        }
     }
 
-    cout << d;
+    li answer = 0;
+    li upper = M/N;
+    sort(d.begin(), d.end());
+    for(li x: d) {
+        if (x > upper) {
+            break;
+        }
+        answer = x;
+    }
+
+    cout << answer << endl;
+
     return 0;
 }
