3f2074f3286d48968e8189377f1a404f13a31cff
hironobu.iga <hironobu.iga.developer@gmail.com>
2020-01-12 15:54:26 +0900

    [add]Leetcode 46 Permutations backtracking

---
+++ Leetcode/46_Permutation/1.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class Solution {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        var nums = nums
+        recurse(0, &nums, &result)
+        return result
+    }
+    
+    func recurse(_ first: Int, _ nums: inout [Int], _ result: inout [[Int]]) {
+        guard first < nums.count else { 
+            result.append(nums) 
+            return
+        }
+        
+        for index in first..<nums.count {
+            nums.swapAt(first, index)
+            self.recurse(first + 1, &nums, &result)
+            nums.swapAt(first, index)
+        }
+    }    
+}
