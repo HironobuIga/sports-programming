ba0567b3b0401eba4da568adbd3266d57a7ec9a0
hironobu.iga <hironobu.iga.developer@gmail.com>
2021-08-19 01:48:39 +0900

    feat: change folder structure

---
+++ AtCoder_Standard90/1_yokan_party/yokan_party.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let inputs = readLine()!.split(separator: " ")
+let N = Int(inputs[0])
+let L = Int(inputs[1])
+let K = Int(readLine()!)
+let breakList = readLine()!.split(separator: " ").map { Int($0) }
+
+
