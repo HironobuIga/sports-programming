e7e2418ba16aff668a283819ca594fd611990314
hironobu.iga <hironobu.iga.developer@gmail.com>
2021-08-19 03:00:01 +0900

    feat: swift answer of 1 yokan party

--- AtCoder_Standard90/1_yokan_party/yokan_party.playground/Contents.swift
+++ AtCoder_Standard90/1_yokan_party/yokan_party.playground/Contents.swift
@@ -1,9 +1,35 @@
 import Foundation
 
 let inputs = readLine()!.split(separator: " ")
-let N = Int(inputs[0])
-let L = Int(inputs[1])
-let K = Int(readLine()!)
-let breakList = readLine()!.split(separator: " ").map { Int($0) }
+let N = Int(inputs[0])!
+let L = Int(inputs[1])!
+let K = Int(readLine()!)!
+let A = readLine()!.split(separator: " ").map { Int($0)! }
 
+func canSolve(m: Int) -> Bool {
+    var count = 0;
+    var preLength = 0;
+    
+    for index in 0..<N {
+        if A[index] - preLength >= m && L - A[index] >= m {
+            count += 1
+            preLength = A[index]
+        }
+    }
+    
+    return count >= K
+}
 
+var left = 0
+var right = L
+
+while right - left > 1 {
+    let middle = (left + right) / 2
+    let solve = canSolve(m: middle)
+    switch solve {
+    case true: left = middle
+    case false: right = middle
+    }
+}
+
+print(left)
